137a0bc43fdd106cf9bad63a0e44119be1455355
Aavash Regmi <avashregmi2065@gmail.com>
2025-10-20 13:40:06 +0545

    Changes

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const donateBtn = document.getElementById("donateBtn");
+  donateBtn.addEventListener("click", () => {
+    alert("🙏 Thank you for your support! This donation helps me continue learning and creating.");
+  });
+});
